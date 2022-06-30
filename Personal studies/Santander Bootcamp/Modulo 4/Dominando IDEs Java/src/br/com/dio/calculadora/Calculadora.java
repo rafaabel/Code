b2996d596d948ee267135d0183b7775d433a6ad8
rafaabel <rafaabel_fgs@hotmail.com>
2022-06-30 12:54:40 -0300

    Criação da Calculadora

--- Personal studies/Santander Bootcamp/Modulo 4/Dominando IDEs Java/src/br/com/dio/calculadora/Calculadora.java
+++ Personal studies/Santander Bootcamp/Modulo 4/Dominando IDEs Java/src/br/com/dio/calculadora/Calculadora.java
@@ -6,17 +6,17 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-		int a, b;
+		double a, b;
 		
 		System.out.println("Digite o primeiro valor: ");
 		a = scan.nextInt();
 		System.out.println("Digite o segundo valor: ");
 		b = scan.nextInt();
 		
-		int soma = soma(a,b);
-		int subtracao = subtracao(a,b);
-		int multiplicacao = multiplicacao(a,b);
-		double divisao = divisao(a,b);5
+		double soma = soma(a,b);
+		double subtracao = subtracao(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		double divisao = divisao(a,b);
 		
 		System.out.println("soma " + soma);
 		System.out.println("subtracao " + subtracao);
@@ -24,16 +24,16 @@ public class Calculadora {
 		System.out.println("divisao " + divisao);
 	}
 	
-	public static int soma (int a, int b ) {
+	public static double soma (double a, double b ) {
 		return a + b;
 	}
-	public static int subtracao (int a, int b ) {
+	public static double subtracao (double a, double b ) {
 		return a - b;
 	}
-	public static double divisao (int a, int b ) {
+	public static double divisao (double a, double b ) {
 		return a / b;
 	}
-	public static int multiplicacao (int a, int b ) {
+	public static double multiplicacao (double a, double b ) {
 		return a * b;
 	}
 
