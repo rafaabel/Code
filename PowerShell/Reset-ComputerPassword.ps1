4b50f2e52a33ed5d9d24e3fe4aa3acc6b542f741
rafaabel <rafaabel_fgs@hotmail.com>
2022-07-14 16:02:19 -0300

    Updating powershell scripts

--- PowerShell/Reset-ComputerPassword.ps1
+++ PowerShell/Reset-ComputerPassword.ps1
@@ -3,16 +3,10 @@ Function Reset-ADComputerMachinePassword {
     param()
 
     $computers = Import-Csv -Path "C:\Users\abelraf\OneDrive - Mars Inc\Documents\Identity Team\Projects\ADDS\PingCastle reports\computerpasswordlastset Mars-AD by LastLogonTimeStamp.csv"
-    $localadmincred = Get-Credential
-    $domaincred = Get-Credential
-
+    $localCredential = New-Object System.Management.Automation.PSCredential -ArgumentList ".\localuser", (ConvertTo-SecureString -String "password" -AsPlainText -Force) #Get-Credential
+    $domainCredential = New-Object System.Management.Automation.PSCredential -ArgumentList 'DOMAIN\domainuser', (ConvertTo-SecureString -String "password" -AsPlainText -Force) #Get-Credential
 
     Foreach ($computer in $computers) {
-        Invoke-Command -ComputerName $computer.Name -Credential $using:localadmincred -ScriptBlock { Reset-ComputerMachinePassword -Credential $using:domaincred }
+        Invoke-Command -ComputerName $computer.Name -Credential $using:localCredential -ScriptBlock { Reset-ComputerMachinePassword -Credential $using:domainCredential }
     }
 }
-
-
-$localCredential = New-Object System.Management.Automation.PSCredential -ArgumentList "domain\user", (ConvertTo-SecureString -String "password" -AsPlainText -Force)
-$domainCredential = New-Object System.Management.Automation.PSCredential -ArgumentList 'domain\user', (ConvertTo-SecureString -String "password" -AsPlainText -Force)
-Invoke-Command -ComputerName LBRGUA97PDK03 -Credential $localCredential -ScriptBlock { Reset-ComputerMachinePassword -Credential $domainCredential }
